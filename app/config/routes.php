1c6903861555212f3176654c8f6763f3a3d4dfa9
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2022-11-16 09:56:18 -0300

    Deleting a route

--- app/config/routes.php
+++ app/config/routes.php
@@ -9,5 +9,4 @@ $router->add('/', array('controller' => 'home', 'action' => 'index'), null, 0)->
 
 $router->add('/:controller/:action', array('controller' => 1, 'action' => 2), null, 100);
 $router->add('/pagina/login', array('controller' => 'login', 'action' => 'index'), null, 11)->setName('pagina/login');
-$router->add('/pagina/autenticacao', array('controller' => 'login', 'action' => 'auth'), null, 11)->setName('/login/auth');
 $router->add('/pagina/profile', array('controller' => 'login', 'action' => 'profile'), null, 11)->setName('pagina/profile');
